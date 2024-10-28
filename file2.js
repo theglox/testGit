fd7f4f9e1ff18f1b867339880a6a7e09c2effdc3
theglox <diegoglox@gmail.com>
2024-10-28 13:14:12 -0500

    commit 7

--- file2.js
+++ file2.js
@@ -1,24 +1,3 @@
 //  hola muindo 
 
 console.log("testing de file 2");
-console.log('Este test es para  verificar el seguimiewnto de ar chivos');
-
-
-console.log("tesdasdsdsa");
-
-
-const lorem = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
-
-//  hola muindo 
-
-console.log("testing de file 2");
-console.log('Este test es para  verificar el seguimiewnto de ar chivos');
-
-
-console.log("tesdasdsdsa");
-
-console.log('Este test es para  verificar el seguimiewnto de ar chivos');
-
-
-console.log("tesdasdsdsa");
-
