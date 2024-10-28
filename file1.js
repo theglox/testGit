7d889c0e6cd0429860af02088c5cfcde0f78c748
theglox <diegoglox@gmail.com>
2024-10-28 13:09:45 -0500

    commit 4

--- file1.js
+++ file1.js
@@ -2,6 +2,16 @@ console.log('Este test es para  verificar el seguimiewnto de ar chivos');
 
 
 console.log("tesdasdsdsa");
+console.log('Este test es para  verificar el seguimiewnto de ar chivos');
 
+
+console.log("tesdasdsdsa");console.log('Este test es para  verificar el seguimiewnto de ar chivos');
+
+
+console.log("tesdasdsdsa");
 
 const lorem = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
+
+
+const lorem3 = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
+const lorem32 = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
--- file2.js
+++ file2.js
@@ -9,14 +9,10 @@ console.log("tesdasdsdsa");
 
 const lorem = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
 
+//  hola muindo 
+
+console.log("testing de file 2");
+console.log('Este test es para  verificar el seguimiewnto de ar chivos');
+
 
 console.log("tesdasdsdsa");
-
-
-const lorem2 = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
-
-
-console.log("tesdasdsdsa");
-
-
-const lorem4 = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
