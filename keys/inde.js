a57f8c827aed8e278824a24fe5b1d09654503de4
theglox <diegoglox@gmail.com>
2024-10-28 13:12:54 -0500

    commit 6

--- keys/inde.js
+++ keys/inde.js
@@ -1,1 +1,25 @@
 console.log("asdasd");
+//  hola muindo 
+
+console.log("testing de file 2");
+console.log('Este test es para  verificar el seguimiewnto de ar chivos');
+
+
+console.log("tesdasdsdsa");
+
+
+const lorem = "lorem aspps tuisd sdn d asdn add a djlsjd  feial ccoomos ddno dhf  dljas fmc0wq "
+
+//  hola muindo 
+
+console.log("testing de file 2");
+console.log('Este test es para  verificar el seguimiewnto de ar chivos');
+
+
+console.log("tesdasdsdsa");
+
+console.log('Este test es para  verificar el seguimiewnto de ar chivos');
+
+
+console.log("tesdasdsdsa");
+
